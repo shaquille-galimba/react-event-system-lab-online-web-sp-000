1656682f4994beda79ba031e7f3d06aecf77ab6d
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-10-30 03:31:09 -0400

    Done.

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,16 @@
 // Code Keypad Component Here
+import React, { Component } from 'react';
+
+class Keypad extends Component {
+	// constructor() {
+	//
+	// }
+
+	render() {
+		return(
+			<input onKeyUp={() => console.log('Entering password...')} type="password"/>
+		)
+	}
+}
+
+export default Keypad
